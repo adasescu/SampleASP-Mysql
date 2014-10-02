67dbdec0398205fef21774b2c33c25a035d955a5
ada.sescu <ada.sescu@uhurusoftware.com>
2014-10-02 13:57:27 +0000

    Fixed creation unnecessary tables bug

--- SampleASPMySQLApp/MySqlInitializer.cs
+++ SampleASPMySQLApp/MySqlInitializer.cs
@@ -1,4 +1,5 @@
-﻿using SampleASPMySQLApp.Models;
+﻿using MySql.Data.MySqlClient;
+using SampleASPMySQLApp.Models;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
@@ -10,29 +11,21 @@ namespace SampleASPMySQLApp
     {
         public void InitializeDatabase(ApplicationDbContext context)
         {
-            if (!context.Database.Exists())
-            {
-                // if database did not exist before - create it
-                context.Database.CreateIfNotExists();
-            }
-            else
-            {
-                string connString = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-                string db = "Database=";
-                string dbName = connString.Substring(connString.LastIndexOf(db) + db.Length, 33);
+            string db = new MySqlConnectionStringBuilder(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString).Database;
 
-                // query to check if MigrationHistory table is present in the database
-                var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
+            // query to check if MigrationHistory table is present in the database
+            var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
                 string.Format(
-                  "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '{0}' AND table_name = '__MigrationHistory'",
-                  dbName));
+                "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '{0}' AND table_name = '__MigrationHistory'",
+                db));
 
+            bool dbMigrated = migrationHistoryTableExists.FirstOrDefault() != 0;
+
+            if (!dbMigrated)
+            {
                 // if MigrationHistory table is not there (which is the case first time we run) - create it
-                if (migrationHistoryTableExists.FirstOrDefault() == 0)
-                {
-                    context.Database.Delete();
-                    context.Database.Create();
-                }
+                context.Database.Delete();
+                context.Database.Create();
             }
         }
     }
